daae31cd0ecfbe428d69c6ff7e6e9c8342634bee
Alexandros Konofagos <akonofagos@jnl>
2018-06-08 13:29:32 +0300

    Correct Matrix transformations

--- Assets/CustomECS/RotationSpeedSystem.cs
+++ Assets/CustomECS/RotationSpeedSystem.cs
@@ -54,17 +54,17 @@ public class RotationSpeedSystem : ComponentSystem {
             p.m0.w = 0; p.m1.w = 0; p.m2.w = 0; p.m3.w = 1.0F;
 
 
-            m.m0.x = 1.0f - (yy + zz); m.m1.x = xy + wz; m.m2.x = xz - wy; m.m3.x = 0.0F;
-            m.m0.y = xy - wz; m.m1.y = 1.0f - (xx + zz); m.m2.y = yz + wx; m.m3.y = 0.0F;
-            m.m0.z = xz + wy; m.m1.z = yz - wx; m.m2.z = 1.0f - (xx + yy); m.m3.z = 0.0F;
+            m.m0.x = 1.0f - (yy + zz); m.m1.x = xy + wz; m.m2.x = xz - wy; m.m3.x = position[i].Value.x;
+            m.m0.y = xy - wz; m.m1.y = 1.0f - (xx + zz); m.m2.y = yz + wx; m.m3.y = position[i].Value.y;
+            m.m0.z = xz + wy; m.m1.z = yz - wx; m.m2.z = 1.0f - (xx + yy); m.m3.z = position[i].Value.z;
             m.m0.w = 0; m.m1.w = 0; m.m2.w = 0; m.m3.w = 1.0F;
 
-			float4 newPos = math.mul(m, new float4 (position[i].Value.x, position[i].Value.y, position[i].Value.z,1));
-			position[i] = new LocalPosition{Value = new float3 (newPos.x, newPos.y, newPos.z)};
+			// float4 newPos = math.mul(m, new float4 (position[i].Value.x, position[i].Value.y, position[i].Value.z,1));
+			// position[i] = new LocalPosition{Value = new float3 (newPos.x, newPos.y, newPos.z)};
 			// m.m0.w = newPos.x; m.m1.w = newPos.y; m.m2.w = newPos.z;
-            m =  math.mul(m,p);
+            m =  math.mul(pM,m);
 			rotation[i] = new LocalRotation {Value = q};
-			position[i] = new LocalPosition{Value = new float3 (position[i].Value.x, position[i].Value.y, position[i].Value.z)};
+			// position[i] = new LocalPosition{Value = new float3 (position[i].Value.x, position[i].Value.y, position[i].Value.z)};
 			transformMatrix[i] = new TransformMatrix{Value = m};
 		}
 	}
@@ -80,6 +80,7 @@ public class RotationSpeedSystem : ComponentSystem {
 		// 	transformMatrix[i] = _data.transformMatrix[i].Value;
 		// 	rotation[i] = _data.rotation[i].Value;
 		// }
+		// Debug.Log(Bootstrap.entityManager.GetComponentData<TransformMatrix>(Bootstrap.transform).Value.m0.x);
 		var job = new RotationSpeedRotation() {dt = Time.deltaTime, 
 		pM = Bootstrap.entityManager.GetComponentData<TransformMatrix>(Bootstrap.transform).Value,
 		position = _data.Position,
